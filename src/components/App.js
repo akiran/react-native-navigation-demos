21e52d289fb9efc2ce6560a096f03bd43c24922a
kiran <kiran@neostack.com>
2017-08-19 00:43:09 +0530

    basic drawer navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
-import {StackNavigator} from 'react-navigation'
+import {StackNavigator, DrawerNavigator} from 'react-navigation'
 import Home from './Home'
 import Profile from './Profile'
 import News from './News'
 
-export default StackNavigator({
+// export default StackNavigator({
+//   Home: {
+//     screen: Home
+//   },
+//   News: {
+//     screen: News
+//   },
+//   Profile: {
+//     screen: Profile
+//   }
+// })
+
+export default DrawerNavigator({
   Home: {
     screen: Home
   },
