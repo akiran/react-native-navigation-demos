cb0b9bec6672dd255c2bbc6609eac69eb9d84f01
kiran <kiran@neostack.com>
2017-08-19 11:11:44 +0530

    header right

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,19 @@ import React from 'react'
 import {
   View,
   Button,
+  TouchableHighlight,
   StyleSheet
 } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default class Home extends React.Component {
   static navigationOptions = ({navigation}) => {
     return {
       title: 'Welcome',
       headerRight: (
-        <Button
-          title='profile'
+        <Ionicons
+          name={'ios-person'}
+          size={26}
           onPress={() => navigation.navigate('Profile', {user: 'John'})}
         />
       )
