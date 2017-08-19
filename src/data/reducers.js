64bda81cdbf25be6df8bd0668a03978e2690c9ca
kiran <kiran@neostack.com>
2017-08-19 11:34:50 +0530

    console.log state and action

--- src/data/reducers.js
+++ src/data/reducers.js
@@ -3,6 +3,7 @@ import { NavigationActions } from 'react-navigation';
 import AppNavigator from '../components/App';
 
 function nav(state, action) {
+  console.log(state, action)
   switch (action.type) {
     default:
       return AppNavigator.router.getStateForAction(action, state);
